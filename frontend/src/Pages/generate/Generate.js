6d9023eded0c2e2462d9f3634ab28aa4d643e3a4
souha <souhaguezguez@gmail.com>
2023-12-17 21:50:23 +0100

    Ajout de la page generate

--- frontend/src/Pages/generate/Generate.js
+++ frontend/src/Pages/generate/Generate.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./Generate.css"
+import Navbar from '../../navbar/Navbar'
 
 export default function Generate() {
   return (
     <div>
-        
-    </div>
+    <Navbar></Navbar>
+    
+    <div className="form-container">
+      <div  className="InputContainer">
+        <input placeholder="Form title" id="input" class="input" name="text" type="text"/>
+      </div>
+    
+    <div className="form-footer">
+    <button className="add-question-btn button" >
+      <span className="text">Add Question</span>
+    </button>
+    <button className="save-form-btn button" >
+      <span className="text">Save</span>
+    </button>
+  </div>
+  </div>
+  </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Router, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Generate from './Pages/generate/Generate';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route
+  } from "react-router-dom";
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+    },
+    {
+        path: "/generate",
+        element: <Generate/>,
+      },
+  ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <RouterProvider router={router}>
+        <Routes>
+            <Route path="/" element={<App/>}/>
+        </Routes>
+    </RouterProvider>
+  
 );
 
